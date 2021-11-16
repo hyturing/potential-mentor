f7e998275b417e060bc257f0ccb2c3ea8a303e61
Hemant Kumar Yadav <hyturing@gmail.com>
2021-11-16 17:14:27 +0530

    https update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
               potential-mentor by hyturing
             </p>
           </div>
-          <form action="http://hyturing.github.io" value="contact">
+          <form action="https://hyturing.github.io" value="contact">
             <button type="submit" className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
               Contact
             </button>
