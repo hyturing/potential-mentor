f6670e4a132349d561d634fd31362c337f4ab887
Hemant Kumar Yadav <hyturing@gmail.com>
2021-11-16 17:12:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           </div>
           <form action="http://hyturing.github.io" value="contact">
             <button type="submit" className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-              Message
+              Contact
             </button>
           </form>
         </div>
